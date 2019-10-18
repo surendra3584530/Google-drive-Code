70b8b6ef47c0cf0eb6bae471be2e67bf1b5c330a
surendra <55162719+surendra3584530@users.noreply.github.com>
2019-10-18 12:28:41 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Client ID and API key from the Developer Console
+  CLIENT_ID: '536077714758-t6topsrfcn0v80plcgakd9tnvss8hqbu.apps.googleusercontent.com',
+  API_KEY: 'AIzaSyCNSsG7Sl-5-iqiT2QHrleMlpIaJmHkGVk',  
+  // Array of API discovery doc URLs for APIs used by the quickstart
+  DISCOVERY_DOCS: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+  // Authorization scopes required by the API; multiple scopes can be
+  // included, separated by spaces.
+  SCOPES: 'https://www.googleapis.com/auth/drive.metadata.readonly'  
 };
 
 /*
